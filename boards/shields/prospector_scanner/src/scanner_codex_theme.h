b4737d16f548436e6fe0ce6991f3247182f40bcd
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-16 14:17:09 +0800

    Use shared scanner host status definition

--- boards/shields/prospector_scanner/src/scanner_codex_theme.h
+++ boards/shields/prospector_scanner/src/scanner_codex_theme.h
@@ -1,14 +1,9 @@
 #pragma once
+
 #include "prospector_layouts.h"
+#include "scanner_host_status.h"
+
 void scanner_codex_theme_create(lv_obj_t *screen);
 void scanner_codex_theme_destroy(void);
 void scanner_codex_theme_update(const struct prospector_keyboard_data *data);
-
-
-struct scanner_host_status {
-    uint8_t codex_left_percent;
-    uint32_t codex_total_tokens;
-    bool codex_available;
-};
-
 void scanner_codex_theme_host_update(const struct scanner_host_status *status);
