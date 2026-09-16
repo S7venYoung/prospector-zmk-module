3860d2d66ce60e501e2daf83ae41631eca0df482
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-16 14:15:13 +0800

    Expose scanner Codex host updates

--- boards/shields/prospector_scanner/src/scanner_codex_theme.h
+++ boards/shields/prospector_scanner/src/scanner_codex_theme.h
@@ -3,3 +3,12 @@
 void scanner_codex_theme_create(lv_obj_t *screen);
 void scanner_codex_theme_destroy(void);
 void scanner_codex_theme_update(const struct prospector_keyboard_data *data);
+
+
+struct scanner_host_status {
+    uint8_t codex_left_percent;
+    uint32_t codex_total_tokens;
+    bool codex_available;
+};
+
+void scanner_codex_theme_host_update(const struct scanner_host_status *status);
