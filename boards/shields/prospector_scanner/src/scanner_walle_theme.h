97c799881014322e76121c32002d549841443de6
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-16 13:48:29 +0800

    feat(scanner): add WALL-E theme interface

---
+++ boards/shields/prospector_scanner/src/scanner_walle_theme.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "prospector_layouts.h"
+void scanner_walle_theme_create(lv_obj_t *screen); void scanner_walle_theme_destroy(void); void scanner_walle_theme_update(const struct prospector_keyboard_data *data);
