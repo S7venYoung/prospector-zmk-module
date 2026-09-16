3c0ea47bb4416078efae693d2bc61b64a5a42245
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-16 13:48:36 +0800

    feat(scanner): add Weather theme interface

---
+++ boards/shields/prospector_scanner/src/scanner_weather_theme.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "prospector_layouts.h"
+void scanner_weather_theme_create(lv_obj_t *screen); void scanner_weather_theme_destroy(void); void scanner_weather_theme_update(const struct prospector_keyboard_data *data);
